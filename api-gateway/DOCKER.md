0837aea120c2b6eadceee1409ab339037abf67ef
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-06-02 23:58:17 +0200

    feat: added config files

---
+++ api-gateway/DOCKER.md
@@ -0,0 +1,3 @@
+# API Gateway
+
+- Repository: [GitHub](https://github.com/polito-WAII-2024/lab5-g13)
--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
+	id("idea")
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.24"
@@ -12,6 +14,13 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+idea {
+	module {
+		isDownloadJavadoc = true
+		isDownloadSources = true
+	}
 }
 
 repositories {
@@ -40,3 +49,22 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+    imageName = "${System.getenv("DOCKER_USERNAME")}/${project.name}:${project.version}"
+    docker {
+        publishRegistry {
+            username = System.getenv("DOCKER_USERNAME")
+            password = System.getenv("DOCKER_PASSWORD")
+        }
+    }
+}
+
+abstract class ProjectVersion : DefaultTask() {
+    @TaskAction
+    fun action() {
+        println(project.version)
+    }
+}
+
+tasks.register<ProjectVersion>("projectVersion")
