6865a93136c260e9697ff2384914295072e7659f
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-06-11 09:41:49 +0200

    added partial documentation

--- api-gateway/src/main/kotlin/it/polito/wa2/apigateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/it/polito/wa2/apigateway/ApiGatewayApplication.kt
@@ -1,9 +1,16 @@
 package it.polito.wa2.apigateway
 
+import it.polito.wa2.apigateway.properties.OpenapiConfigProperties
+import it.polito.wa2.apigateway.properties.ProjectConfigProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(
+	ProjectConfigProperties::class,
+	OpenapiConfigProperties::class,
+)
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
