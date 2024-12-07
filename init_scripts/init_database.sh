bb73536b752797bcefe0e4eca0894fe458ac4daf
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-12-07 10:42:22 +0100

    feat: added debezium for data capture

--- init_scripts/init_database.sh
+++ init_scripts/init_database.sh
@@ -14,6 +14,29 @@ CREATE DATABASE $DB OWNER $DB;
 EOSQL
 }
 
+function create_debezium() {
+  local DB=$1
+  local PASSWORD=$2
+
+  echo "Creating user for debezium and altering the WriteAheadLog level."
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+ALTER SYSTEM SET wal_level = logical;
+SELECT pg_reload_conf();
+CREATE USER $DB WITH REPLICATION ENCRYPTED PASSWORD '${PASSWORD}';
+ALTER USER $DB WITH SUPERUSER;
+EOSQL
+}
+
+function grant_debezium_permissions() {
+  local DEBEZIUM_DB=$1
+  local OTHER_DB=$2
+
+  echo "Granting permission to debezium on ${OTHER_DB}"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+GRANT USAGE ON DATABASE ${OTHER_DB} SCHEMA public TO ${DEBEZIUM_DB};
+EOSQL
+}
+
 if [ -z "$POSTGRES_MULTIPLE_DB" ]; then
   echo "POSTGRES_MULTIPLE_DB was not set!"
   exit 1
@@ -39,3 +62,7 @@ for (( index=0; index<${#DBS[@]}-1; index++ )); do
 done
 
 echo "Multiple database creation finished"
+
+echo "Initializing database for debezium support"
+create_debezium "$DEBEZIUM_DB" "$DEBEZIUM_PASSWORD"
+echo "Debezium support initialization completed!"
